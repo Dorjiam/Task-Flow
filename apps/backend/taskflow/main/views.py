b0b9abc20dcd734429b238b35b8dfa723821f5f4
Dorjiam <DorjiamNotKnown@gmail.com>
2024-05-11 22:24:43 +0330

    production commit

--- apps/backend/taskflow/main/views.py
+++ apps/backend/taskflow/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+def index(request):
+    return HttpResponseRedirect("/home")
+
+
+def home(request):
+    return render(request, "home.html")
--- apps/backend/taskflow/taskflow/urls.py
+++ apps/backend/taskflow/taskflow/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from signup import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path("signup/", v.signup, name="signup"),
     path("", include("main.urls")),
 ]
